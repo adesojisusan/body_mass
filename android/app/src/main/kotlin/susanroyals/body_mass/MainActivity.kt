9ed8b92278b02085b0700c13447c3b13edf99c73
Susan Adesoji <adesojisusan.adeola@gmail.com>
2020-09-15 14:14:21 +0100

    commit

---
+++ android/app/src/main/kotlin/susanroyals/body_mass/MainActivity.kt
@@ -0,0 +1,12 @@
+package susanroyals.body_mass
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BodyMass());
+ class BodyMass extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return Container();
+   }
+ }
+ 
