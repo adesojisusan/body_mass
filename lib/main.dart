d931e4e1931ecfd8bb8ccfc0bb802fad301a8f56
Susan Adesoji <adesojisusan.adeola@gmail.com>
2020-09-15 17:08:56 +0100

    second commits

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(BodyMass());
+
+
  class BodyMass extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+      theme:ThemeData(
+        primaryColor:Color(0xff030627),
+        scaffoldBackgroundColor: Color(0xff030627),
+          textTheme: TextTheme(body1:TextStyle(
+            color: Colors.white,
+          ),
+          ),
+      ),
+     );
+   }
+ }
+ class InputPage extends StatefulWidget {
+   @override
+   _InputPageState createState() => _InputPageState();
+ }
+
+ class _InputPageState extends State<InputPage> {
    @override
    Widget build(BuildContext context) {
      return Container();
    }
  }
- 
