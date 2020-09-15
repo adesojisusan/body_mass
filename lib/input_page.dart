4e69023536febdfb5fa3e056fb28559d31e0eb6b
Susan Adesoji <adesojisusan.adeola@gmail.com>
2020-09-15 17:53:58 +0100

    U.i refractory

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,6 +9,54 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+    children: <Widget>[
+      Expanded(child: Row(
+        children: <Widget>[
+          Expanded(child: Container(
+        color: Color(0xff02063F),
+         margin: EdgeInsets.all(15.0),
+          decoration:BoxDecoration(
+             borderRadius:BorderRadius.circular(10.0)
+              ) ,
+               ),),
+                Expanded(child: Container(
+                 color: Color(0xff02063F),
+                    margin: EdgeInsets.all(15.0),
+              decoration:BoxDecoration(
+              borderRadius:BorderRadius.circular(10.0)
+                      ) ,
+                  ),
+          ),
+        ],
+      ),
+      ),
+      Expanded(child: Container(
+        color: Color(0xff02063F),
+        margin: EdgeInsets.all(15.0),
+        decoration:BoxDecoration(
+          borderRadius:BorderRadius.circular(10.0)
+        ) ,
+      ),),
+      Expanded(child: Row(
+        children: <Widget>[
+          Expanded(child: Container(
+            color:Color(0xff02063F),
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              borderRadius:BorderRadius.circular(10.0)
+            ),
+          ),),
+          Expanded(child: Container(
+            color: Color(0xff02063F),
+            margin: EdgeInsets.all(15.0),
+            decoration:BoxDecoration(
+                borderRadius:BorderRadius.circular(10.0)
+            ) ,
+          ),),
+        ],
+      ),),
+    ],
+    );
   }
 }
